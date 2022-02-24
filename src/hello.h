4db8f9fea08900079db1ff1ebb1f0b2b436b15e7
Dan Ellis Echavarria <danellise@outlook.com>
2022-02-23 22:14:50 -0500

    Bonus Points

--- src/hello.h
+++ src/hello.h
@@ -1,9 +1,10 @@
 #ifndef HELLO_WORLD_S
 #define HELLO_WORLD_S
-#include <stdio.h>
 void helloWorld();
 
 #ifdef HELLO_WORLD_IMPL
+#include <stdio.h>
+
 void helloWorld()
 {
    printf("Hello World from a single header file\n");
